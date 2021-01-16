9a2c42726c2e52eeb6c158519590fc3c3c1acc98
junni3h <junni3h@naver.com>
2021-01-16 23:16:53 +0900

    first commit

---
+++ src/main/java/com/insummnia/webpjt/WebpjtApplication.java
@@ -0,0 +1,13 @@
+package com.insummnia.webpjt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebpjtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebpjtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/insummnia/webpjt/WebpjtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.insummnia.webpjt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebpjtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
